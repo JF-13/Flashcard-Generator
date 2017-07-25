02cc9319ac9ac5e0f5488d9daf89aefe15bcaa93
acer <jf13@mb.lan>
2017-07-24 21:51:56 -0500

    'stuck at error but almost done'

---
+++ BasicCard.js
@@ -0,0 +1,7 @@
+
+function BasicCard (fron, back) {
+  this.front = front;
+  this.back = back;
+}
+
+module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,11 @@
+function cloze(text, cloze) {
+  if (text === undefined || cloze === undefined) {
+    console.log("error, youre missing something");
+  } else {
+    this.cloze = cloze;
+    this.partial = text;
+    this.fullText = fullText;
+  }
+}
+
+module.exports = ClozeCard;
