ff78f8ca4a7612bdd1fc5caea66e7feeb21610df
acer <jf13@mb.lan>
2017-07-25 14:43:55 -0500

    'I think im done!'

--- BasicCard.js
+++ BasicCard.js
@@ -1,7 +1,22 @@
 
-function BasicCard (fron, back) {
-  this.front = front;
-  this.back = back;
+function BasicCard (front, back) {
+  if (front === undefined || back === undefined) {
+    console.log("error, youre missing ... try again");
+  } else {
+    this.front = front;
+    this.back = back;
+    this.showFront = function() {
+      console.log(this.front);
+    };
+    this.showBack = function() {
+      console.log(this.back);
+    };
+  }
 }
+
+//EXAMPLE
+// var card1 = new BasicCard('George Washington', 'Is the first US president');
+// card1.showFront();
+// card1.showBack();
 
 module.exports = BasicCard;
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,11 +1,20 @@
 function cloze(text, cloze) {
   if (text === undefined || cloze === undefined) {
-    console.log("error, youre missing something");
+    console.log("error, youre missing ... try again");
   } else {
     this.cloze = cloze;
     this.partial = text;
-    this.fullText = fullText;
+    this.fullText = function() {
+      var full = this.partial.replace("...", this.cloze);
+      console.log(full);
+    };
   }
 }
 
-module.exports = ClozeCard;
+// EXAMPLE
+// var fanta = new cloze('love is all you ...', 'need');
+// console.log(fanta.partial);
+// console.log(fanta.cloze);
+// fanta.fullText();
+
+module.exports = cloze;
